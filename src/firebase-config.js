41b723c80e763e58ce3b3754945ef4568a24b44e
lukonianlogic <lucas.longacre@gmail.com>
2024-03-30 19:48:51 -0600

    Updated Firebase configuration and added to .env for security

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-fO2F5eTOGxIX2tYX14oCEtyLwsp5vyU",
-  authDomain: "ninjaapp-55342.firebaseapp.com",
-  projectId: "ninjaapp-55342",
-  storageBucket: "ninjaapp-55342.appspot.com",
-  messagingSenderId: "157953523641",
-  appId: "1:157953523641:web:90b93f1938b3183b8f8068",
-  measurementId: "G-J21HMHLQDV"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Only initialize analytics if measurementId exists
+let analytics;
+if (firebaseConfig.measurementId) {
+  analytics = getAnalytics(app);
+}
